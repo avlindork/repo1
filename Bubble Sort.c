26ba44590799f8da2360aaaec7d3f54f3cbc795c
Akash <56739695+Akash2709@users.noreply.github.com>
2022-10-31 08:17:58 +0530

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -8,7 +8,7 @@ void swap(int *xp, int *yp)
     *yp = temp;
 }
  
-// A function to implement bubble sort
+// A function to implement   bubble sort
 void bubbleSort(int arr[], int n)
 {
     int i, j;
